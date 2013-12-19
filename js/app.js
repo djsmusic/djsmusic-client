e0c4b410968b36ce21785423e4763c1fc9df1d22
aurbano <alejandro@urbanoalvarez.es>
2013-12-19 19:00:25 +0100

    RequireJS working fine

--- js/app.js
+++ js/app.js
@@ -6,79 +6,37 @@
  * @extends Backbone.View
  * @author Alejandro U. Álvarez <alejandro@urbanoalvarez.es>
  */
+require.config({
 
-define(['jquery', 'backbone', 'shell'], function($, Backbone, ShellView) {
-	
-	var directory = {
-	
-	    views: {},
-	
-	    models: {},
-	
-	    loadTemplates: function(views, callback) {
-	
-	        var deferreds = [];
-	
-	        $.each(views, function(index, view) {
-	            //if (directory[view]) {
-	                deferreds.push($.get('tpl/' + view + '.html', function(data) {
-	                	directory[view] = {prototype: {}};
-	                    directory[view].prototype.template = _.template(data);
-	                }, 'html'));
-	            //} else {
-	            //    alert(view + " not found");
-	            //}
-	        });
-	
-	        $.when.apply(null, deferreds).done(callback);
-	    }
-	
-	};
-	
-	directory.Router = Backbone.Router.extend({
-	
-	    routes: {
-	        "":                 "home",
-	        "contact":          "contact"
-	    },
-	
-	    initialize: function () {
-	    	console.log('App init');
-	        directory.shellView = new ShellView();
-	        $('body').html(directory.shellView.render().el);
-	        // Close the search dropdown on click anywhere in the UI
-	        $('body').click(function () {
-	            $('.dropdown').removeClass("open");
-	        });
-	        this.$content = $("#content");
-	        this.$player = $("#player");
-	        
-	        directory.PlayerView = new directory.PlayerView();
-	        this.$player.html(directory.PlayerView.render().el);
-	    },
-	
-	    home: function () {
-	        // Since the home view never changes, we instantiate it and render it only once
-	        if (!directory.homelView) {
-	            directory.homelView = new directory.HomeView();
-	            directory.homelView.render();
-	        } else {
-	            console.log('reusing home view');
-	            directory.homelView.delegateEvents(); // delegate events when the view is recycled
-	        }
-	        this.$content.html(directory.homelView.el);
-	        directory.shellView.selectMenuItem('home-menu');
-	    },
-	
-	    contact: function () {
-	        if (!directory.contactView) {
-	            directory.contactView = new directory.ContactView();
-	            directory.contactView.render();
-	        }
-	        this.$content.html(directory.contactView.el);
-	        directory.shellView.selectMenuItem('contact-menu');
-	    }
-	});
-	
-	return directory;
+    baseUrl: 'js/lib',
+
+    paths: {
+        app: '../app',
+        tpl: '../tpl',
+        jquery : '../lib/jquery-1.9.1.min',
+        backbone : '../lib/backbone-min',
+        text : '../lib/text',
+        underscore : '../lib/underscore-min'
+    },
+
+    map: {
+        '*': {
+            'app/models/employee': 'app/models/memory/employee'
+        }
+    },
+
+    shim: {
+        'backbone': {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        },
+        'underscore': {
+            exports: '_'
+        }
+    }
+});
+
+require(['jquery', 'backbone', 'app/router'], function ($, Backbone, Router) {
+    var router = new Router();
+    Backbone.history.start();
 });
