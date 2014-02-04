d5bdf1a790fd5b459bdd7ddca546b853f663a5a5
aurbano <alejandro@urbanoalvarez.es>
2014-02-04 10:27:54 +0100

    Improved API support

--- js/app/views/DJ_Songs.js
+++ js/app/views/DJ_Songs.js
@@ -2,14 +2,16 @@ define(function (require) {
 
     "use strict";
     
-    var $           = require('jquery'),
-        _           = require('underscore'),
-        Backbone    = require('backbone'),
-        tpl         = require('text!tpl/DJ_Songs.html'),
-        Player		= require('app/views/Player'),
-        Display		= require('display'),
-        Songs		= require('app/collections/songs'),
-        SongListView		= require('app/views/SongList'),
+    var $           	= require('jquery'),
+        _           	= require('underscore'),
+        Backbone    	= require('backbone'),
+        tpl         	= require('text!tpl/DJ_Songs.html'),
+        Player			= require('app/views/Player'),
+        Display			= require('display'),
+        Songs			= require('app/collections/songs'),
+        Albums			= require('app/collections/albums'),
+        SongListView	= require('app/views/SongList'),
+        AlbumListView	= require('app/views/AlbumList'),
 
         template = _.template(tpl);
 
@@ -18,21 +20,35 @@ define(function (require) {
     	initialize: function(data){
     		console.log('DJ Songs: init ID '+this.model.attributes.artist.id);
     		this.model.on("change", this.render);
-    		// Request the list of songs in the album
+    		// Artist ID
 			var artistId = this.model.attributes.artist.id;
+			// Get the artists songs
        		this.songs = new Songs();
     		this.songs.fetch({
     			data: {
     				user: artistId
     			}
     		});
+    		// Get the artists albums
+    		this.albums = new Albums();
+    		this.albums.fetch({
+    			data: {
+    				user: artistId
+    			},
+    			success: function(mod,data){
+    				console.log('Albums: Fetched ',data);
+    			}
+    		});
+    		// Create new list views
     		this.songList = new SongListView({collection : this.songs});
+    		this.albumList = new AlbumListView({collection : this.albums});
     	},
 
         render: function () {
         	this.$el.html(template(this.model.attributes));
         	
         	$('#songs').append(this.songList.render().el);
+        	$('#albums').append(this.albumList.render().el);
         	
             return this;
         },
