001731e6e79702c16ad43ddfd7d743a009e2d6ef
Alejandro U. Alvarez <alejandro@urbanoalvarez.es>
2014-02-04 16:30:22 +0100

    SongList: Better styling

--- js/app/views/SongListItem.js
+++ js/app/views/SongListItem.js
@@ -13,13 +13,14 @@ define(function (require) {
 
     return Backbone.View.extend({
 
-        tagName: "tr",
+        tagName: "li",
 
         initialize: function () {
         	this.model.on("change", this.render, this);
         },
 
         render: function () {
+        	this.$el.addClass('list-group-item');
         	this.model.attributes.track.ratingStars = Display.rating(this.model.attributes.track.rating);
         	this.model.attributes.track.durationString = Display.timeToString(this.model.attributes.track.duration);
         	this.$el.html(template(this.model.attributes));
