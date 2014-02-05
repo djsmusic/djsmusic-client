d5150277e2c53c5a1c28abcaaf3e129af95ab00e
Alejandro U. Alvarez <alejandro@urbanoalvarez.es>
2014-02-05 16:55:16 +0100

    Search improvements

--- js/app/views/SongList.js
+++ js/app/views/SongList.js
@@ -14,16 +14,28 @@ define(function (require) {
 		initialize: function () {
 			console.log("SongList: Init");
 			this.collection.on("reset", this.render, this);
+			this.collection.on("fetched", this.fetched, this);
 			this.collection.on("add", this.renderOne, this);
 		},
 		
 		render: function () {
 			this.$el.empty().addClass('list-group');
+			
 			_.each(this.collection.models, function (song) {
 				this.$el.append(new SongListItemView({model: song}).render().el);
 			}, this);
-		
+			
 			return this;
+		},
+		
+		fetched: function(){
+			if(this.collection.length > 0){
+				_.each(this.collection.models, function (song) {
+					this.$el.append(new SongListItemView({model: song}).render().el);
+				}, this);
+			}else{
+				this.$el.append('<li class="media"><div class="alert alert-warning">No results</div></li>');
+			}
 		},
 		       
 		renderOne: function (model) {
