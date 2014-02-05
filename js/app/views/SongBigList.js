0439a42be1c3a8dc60c43503b8c18ed557968db5
Alejandro U. Alvarez <alejandro@urbanoalvarez.es>
2014-02-05 19:53:05 +0100

    BigList: Simple pagination

--- js/app/views/SongBigList.js
+++ js/app/views/SongBigList.js
@@ -5,38 +5,86 @@ define(function (require) {
     var $                       = require('jquery'),
         _                       = require('underscore'),
         Backbone                = require('backbone'),
-        SongListItemView    	= require('app/views/SongBigListItem');
+        tpl                 	= require('text!tpl/SongBigList.html'),
+        SongListItemView    	= require('app/views/SongBigListItem'),
+        
+        template = _.template(tpl);
 
     return Backbone.View.extend({
     	
-    	tagName: 'div',
-
-        initialize: function () {
+    	initialize: function () {
         	console.log("SongBigList: Init");
-        	this.collection.on("reset", this.render, this);
+        	
+        	this.collection.on("reset", this.renderAgain, this);
         	this.collection.on("fetched", this.check, this);
             this.collection.on("add", this.renderOne, this);
         },
+        
+        events: {
+    		'change .BigListFilter' : 'filter',
+    		'click .pager a' : 'paging'
+    	},
 
         render: function () {
-        	this.$el.empty().addClass('big-list');
+        	this.$el.empty().addClass('big-list').html(template());
+        	
+        	this.$list = this.$el.find('.SongBigList');
         	
         	_.each(this.collection.models, function (song) {
-				this.$el.append(new SongListItemView({model: song}).render().el);
+				this.$list.append(new SongListItemView({model: song}).render().el);
 			}, this);
             
             return this;
         },
         
+        renderAgain: function () {
+        	this.$list.empty();
+        	
+        	_.each(this.collection.models, function (song) {
+				this.$list.append(new SongListItemView({model: song}).render().el);
+			}, this);
+            
+            return this;
+        },
+        
+        paging: function(e){
+        	var type = $(e.currentTarget).attr('rel'),
+				current = this.collection.meta('page');
+			if(typeof(current)==='undefined' || current < 1) current = 1;
+			switch(type){
+				case 'prev':
+					current--;
+					break;
+				case 'next':
+					current++;
+					break;
+			}
+			if(current<2){
+				current = 1;
+				$('.pager li:first-child').addClass('disabled');
+			}
+			if(current>1) $('.pager li:first-child').removeClass('disabled');
+			this.collection.meta('page',current);
+			$('.pager small').text(current);
+			
+			return this;
+        },
+        
+        filter: function(e){
+        	console.log('BigList filter');
+        	this.collection.meta($(e.currentTarget).attr('name'), $(e.currentTarget).val());
+        	return this;   	
+        },
+        
         check: function(){
         	if(this.collection.length == 0){
-				this.$el.html('<div class="media"><div class="alert alert-warning"><i class="fa fa-exclamation-triangle"></i> No results</div></div>');
+				this.$list.html('<div class="media"><div class="alert alert-warning"><i class="fa fa-exclamation-triangle"></i> No results</div></div>');
 			}
 			return this;
         },
 		       
 		renderOne: function (model) {
-			this.$el.append(new SongListItemView({model: model}).render().el);
+			this.$list.append(new SongListItemView({model: model}).render().el);
 			return this;
 		}
     });
