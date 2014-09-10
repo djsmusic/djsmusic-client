deee81cc41cb9b8dc6b6b7f8cd75b8546eec4c7e
Alejandro U. Alvarez <alejandro@urbanoalvarez.es>
2014-09-10 17:05:33 +0200

    Formatting

--- js/app/app.js
+++ js/app/app.js
@@ -1,19 +1,20 @@
 define(function (require, exports, module) {
-	
-	console.log('App: init');
-	
-	var $           = require('jquery'),
-        Backbone    = require('backbone'),
 
-    	app = {
-	        productionUrl: 'http://api.djs-music.com/',
-	    	localUrl: 'http://127.0.0.1/djsmusic/',
-	    	url: '',
-	    	mode: 0,	// API mode: 1= Testing, 0= Production
-	
-	        // Show alert classes and hide after specified timeout
-	        showAlert: function(title, text, klass) {
-	        	alert(text);/*
+	console.log('App: init');
+
+	var $ = require('jquery'),
+		Backbone = require('backbone'),
+
+		app = {
+			productionUrl: 'http://api.djs-music.com/',
+			localUrl: 'http://127.0.0.1/djsmusic/',
+			url: '',
+			mode: 1, // API mode: 1= Testing, 0= Production
+
+			// Show alert classes and hide after specified timeout
+			showAlert: function (title, text, klass) {
+				alert(text);
+				/*
 	            $("#header-alert").removeClass("alert-error alert-warning alert-success alert-info");
 	            $("#header-alert").addClass(klass);
 	            $("#header-alert").html('<button class="close" data-dismiss="alert">×</button><strong>' + title + '</strong> ' + text);
@@ -21,32 +22,34 @@ define(function (require, exports, module) {
 	            setTimeout(function() {
 	                $("#header-alert").hide();
 	            }, 7000 );*/
-	        },
-	        
-	        /**
-	         * Capture AJAX requests and display a global nanobar for them 
-	         */
-	        startLoading: function(){
-	        	if(typeof(this.nanobar)=='undefined') return;
-	        }
-	    };
-	
+			},
+
+			/**
+			 * Capture AJAX requests and display a global nanobar for them
+			 */
+			startLoading: function () {
+				if (typeof (this.nanobar) == 'undefined') return;
+			}
+		};
+
 	// force ajax call on all browsers
-    $.ajaxSetup({ cache: false });
-    
-    app.url = app.productionUrl;
-    
-    if(app.mode == 1){
-    	console.info('API: Local mode');
-    	app.url = app.localUrl;
-    }else{
-    	// Disable console logging for production mode.
-    	//console.log = function(){};
-    }
+	$.ajaxSetup({
+		cache: false
+	});
 
-    // Global event aggregator
-    app.eventAggregator = _.extend({}, Backbone.Events);
+	app.url = app.productionUrl;
 
-    return app;
+	if (app.mode == 1) {
+		console.info('API: Local mode');
+		app.url = app.localUrl;
+	} else {
+		// Disable console logging for production mode.
+		//console.log = function(){};
+	}
+
+	// Global event aggregator
+	app.eventAggregator = _.extend({}, Backbone.Events);
+
+	return app;
 
 });
